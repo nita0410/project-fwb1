f6b068272cd39b4e40b65bbe5ee9ca58f42a53f9
nita0410 <naranita78@gmail.com>
2025-05-08 23:40:41 +0800

    first commit

---
+++ database/migrations/2025_05_08_144354_incoming.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('incoming_items', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('item_id')->constrained('items')->onDelete('cascade');
+            $table->integer('quantity');
+            $table->string('source')->nullable();
+            $table->date('date_in');
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            $table->timestamps();
+        });
+        
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('incoming_items');
+    }
+};
